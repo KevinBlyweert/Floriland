7ca494b85f008f2fe2fcee2de4c602c89b28c748
KevinBlyweert <kevin.blyweert@oclock.school>
2023-07-07 13:59:49 +0200

    fix/exo

--- server.js
+++ server.js
@@ -2,18 +2,22 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 const flowers = require("./flowers");
+const functions = require("./functions");
+app.use(express.static(__dirname));
 app.get("/",(req,res)=>{
-    res.sendFile(`${__dirname}/index.html`);
+    res.send(`${__dirname}/index.html`);
 });
 app.get("/fleurs",(req,res)=>{
-    let HTMLResponse ="<ul>";
+    let HTMLResponse =functions.setHeader();
+    HTMLResponse +="<ul>";
+    // let HTMLResponse ="<ul>";
     flowers.forEach((element)=>{
         HTMLResponse += `<li><a href="${req.url}/${element}">${element}</a></li>`;
     });
-    HTMLResponse +="</ul><br><a href='/'>Retour à l'accueil</a>";
+    HTMLResponse +="</ul></main><div id='links'><a href='/'>Retour à l'accueil</a></div>";
     res.send(HTMLResponse);
 });
 app.get("/fleurs/:id",(req,res)=>{
-    res.send(`Vous avez choisi la fleur : ${req.params.id}<br><a href='/'>Retour à l'accueil</a>`);
+    res.send(`${functions.setHeader()}Vous avez choisi la fleur : ${req.params.id}</main><div id="links"><a href='/'>Retour à l'accueil</a><a href='/fleurs'>Retour aux fleurs</a></div>`);
 });
 app.listen(PORT);
