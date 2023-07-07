52957212ec0b912afc1365575bc468d989a40cd2
KevinBlyweert <kevin.blyweert@oclock.school>
2023-07-07 12:18:55 +0200

    fix/exo

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const app = express();
+const PORT = 3000;
 const flowers = require("./flowers");
 app.get("/",(req,res)=>{
-    res.sendFile("/home/student/Bureau/floriland/index.html");
+    res.sendFile(`${__dirname}/index.html`);
 });
 app.get("/fleurs",(req,res)=>{
     let HTMLResponse ="<ul>";
     flowers.forEach((element)=>{
-        HTMLResponse += `<li>${element}</li>`;
+        HTMLResponse += `<li><a href="${req.url}/${element}">${element}</a></li>`;
     });
     HTMLResponse +="</ul><br><a href='/'>Retour à l'accueil</a>";
     res.send(HTMLResponse);
 });
-app.listen(3000);
+app.get("/fleurs/:id",(req,res)=>{
+    res.send(`Vous avez choisi la fleur : ${req.params.id}<br><a href='/'>Retour à l'accueil</a>`);
+});
+app.listen(PORT);
