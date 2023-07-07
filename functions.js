8b2d6527bdc50c51a9ac441b7fb3ed75d2a8d311
KevinBlyweert <kevin.blyweert@oclock.school>
2023-07-07 14:31:52 +0200

    fix/exo

--- functions.js
+++ functions.js
@@ -6,8 +6,8 @@ module.exports = {
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Floriland - Bienvenue!</title>
             <link rel="stylesheet" href="/css/style.css">
+            <title>Floriland - Bienvenue!</title>
         </head>
         <body>
             <header>
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const flowers = require("./flowers");
 const functions = require("./functions");
 app.use(express.static(__dirname));
 app.get("/",(req,res)=>{
-    res.send(`${__dirname}/index.html`);
+    res.send(`${functions.setHeader}${__dirname}/index.html`);
 });
 app.get("/fleurs",(req,res)=>{
     let HTMLResponse =functions.setHeader();
@@ -14,10 +14,10 @@ app.get("/fleurs",(req,res)=>{
     flowers.forEach((element)=>{
         HTMLResponse += `<li><a href="${req.url}/${element}">${element}</a></li>`;
     });
-    HTMLResponse +="</ul></main><div id='links'><a href='/'>Retour à l'accueil</a></div>";
+    HTMLResponse +="</ul></main><footer id='links'><a href='/'>Retour à l'accueil</a></footer>";
     res.send(HTMLResponse);
 });
 app.get("/fleurs/:id",(req,res)=>{
-    res.send(`${functions.setHeader()}Vous avez choisi la fleur : ${req.params.id}</main><div id="links"><a href='/'>Retour à l'accueil</a><a href='/fleurs'>Retour aux fleurs</a></div>`);
+    res.send(`${functions.setHeader()}Vous avez choisi la fleur : ${req.params.id}</main><footer id="links"><a href='/'>Retour à l'accueil</a><a href='/fleurs'>Retour aux fleurs</a></footer>`);
 });
 app.listen(PORT);
